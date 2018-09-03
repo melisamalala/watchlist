a482dd18f0db3d148857679b0945364ce4a7c1dc
melisamalala <melissamalala@gmail.com>
2018-09-03 16:01:23 +0300

    Added password authentication and User login

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,12 @@ from flask import Flask
 from flask_bootstrap import Bootstrap
 from config import config_options
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+
+
+login_manager = LoginManager()
+login_manager.session_protection = 'strong'
+login_manager.login_view = 'auth.login'
 
 bootstrap = Bootstrap()
 db = SQLAlchemy()
@@ -15,16 +21,22 @@ def create_app(config_name):
 
     app.config.from_object(config_options[config_name])
     config_options[config_name].init_app(app)
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 
     # Initializing flask extensions
     bootstrap.init_app(app)
     db.init_app(app)
+    login_manager.init_app(app)
 
 
     # Registering the blueprint
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
+
+    # Registering authentication blueprint
+    from.auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint,url_prefix = '/authenticate')
 
     # setting config
     from .request import configure_request
