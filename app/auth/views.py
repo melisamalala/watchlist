9ed1574ef7f18270b67748e19c834909d945c6d3
melisamalala <melissamalala@gmail.com>
2018-09-04 12:36:26 +0300

    User email notification working

--- app/auth/views.py
+++ app/auth/views.py
@@ -9,7 +9,7 @@ from ..email import  mail_message
 @auth.route('/login', methods = ['GET', 'POST'])
 def login():
     login_form=LoginForm()
-    if login_form.validte_on_submit():
+    if login_form.validate_on_submit():
         user=User.query.filter_by(email = login_form.email.data).first()
         if user is not None and user.verify_password(login_form.password.data):
             login_user(user,login_form.remember.data)
@@ -19,22 +19,8 @@ def login():
 
 
     title = "watchlist login"
-    render_template('auth/login.html', login_form= login_form, title =title)
+    return render_template('auth/login.html', login_form= login_form, title =title)
 
-@auth.route('/register', methods = ["GET", "POST"])
-def register():
-    form = RegistrationFrom()
-    if form.validate_on_submit():
-        user = User(email = form.email.data,
-                    username =form.username.data,
-                    password= form.password.data)
-
-        db.session.add(user)
-        db.session.commit()
-        return redirect(url_for('auth.login'))
-        title = "New Account"
-
-    return render_template('auth/register.html',registration_form =form)
 
 @auth.route('/logout')
 @login_required
@@ -58,8 +44,9 @@ def register():
                      user.email,
                      user=user)
 
-        return redirect(url_for('auth.login'))
         title = "New Account"
+        return redirect(url_for('auth.login'))
+
 
     return render_template('auth/register.html', registration_form=form)
 
