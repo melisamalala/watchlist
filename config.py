2cd5542c254187c134e515fd56bdd779ed2ffaed
melisamalala <melissamalala@gmail.com>
2018-09-04 12:14:10 +0300

    Added email files to send welcome email to user

--- config.py
+++ config.py
@@ -6,6 +6,16 @@ class Config:
     MOVIE_API_KEY = os.environ.get('MOVIE_API_KEY')
     SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://melissamalala:abc123@localhost/watchlist'
+    UPLOADED_PHOTOS_DEST = 'app/static/photos'
+
+
+    # email configurations
+
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
 
 
     @staticmethod
--- start.sh
+++ start.sh
@@ -1,4 +1,8 @@
-export MOVIE_API_KEY='445e94a0a312025e243a123fb0683e31'
-export SECRET_KEY= 'abc123'
+export MOVIE_API_KEY=445e94a0a312025e243a123fb0683e31
+export SECRET_KEY=abc123
+
+
+export MAIL_USERNAME=moringamelissa254@gmail.com
+export MAIL_PASSWORD=moringa123
 
 python3.6 manage.py server
